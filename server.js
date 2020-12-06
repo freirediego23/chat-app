12cfbbfdc3b965a88145a5d5344d034ccdf339ca
freirediego23 <diegofg23@outlook.es>
2020-12-05 22:42:28 -0500

    add

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const io = require('socket.io')(server)
 app.set('views', './views')
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'views')))
 app.use(express.urlencoded({ extended: true }))
 
 const rooms = { }
