af87aec699b132a58c598a429982bfffd21950ab
freirediego23 <diegofg23@outlook.es>
2020-12-05 22:51:34 -0500

    add

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const io = require('socket.io')(server)
 
 app.set('views', './views')
 app.set('view engine', 'ejs')
-app.use(express.static('public'))
+//app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'views')))
 
